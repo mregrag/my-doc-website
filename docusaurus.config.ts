c15218ae784975b95c307893d46d784e9a314786
Mohamed Regrag <mregrag@e3r3p3.1337.ma>
2024-10-21 01:40:09 +0100

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,19 +11,16 @@ const config: Config = {
   url: 'https://mregrag.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-doc-website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mregrag', // Usually your GitHub org/user name.
   projectName: 'my-doc-website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  deploymentBranch: "gh-pages",
-  trailingSlash: false,
-
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
